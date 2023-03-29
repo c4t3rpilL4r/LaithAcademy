d86e233f8e653251affdc4abc69fba72ea38a23a
root <root@Ardii>
2023-03-29 13:47:21 +0800

    Counter App Chapter - DONE

---
+++ Counter App/Vue/VueCounterApp/src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+const addToCount = () => {
+  count.value = count.value + 1;
+};
+const subtractFromCount = () => {
+  count.value = count.value - 1;
+};
+</script>
+
+<template>
+  <main>
+    <div>
+      <h4>The current count is ...</h4>
+      <h1>{{ count }}</h1>
+      <button @click="addToCount">-</button>
+      <button @click="subtractFromCount">+</button>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+main {
+  background-color: aliceblue;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+div {
+  text-align: center;
+}
+</style>
