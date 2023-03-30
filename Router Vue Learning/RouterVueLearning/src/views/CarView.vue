0742de9bd6bb23dfc6e6e59e82ac25691030e0c8
Raphael de Arce <56951197+mllrdev-raphael-dearce@users.noreply.github.com>
2023-03-30 18:48:23 +0800

    wip: 4:46:40

---
+++ Router Vue Learning/RouterVueLearning/src/views/CarView.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { useRoute, useRouter, RouterView } from "vue-router";
+import cars from "../data/cars.json";
+
+const route = useRoute();
+const router = useRouter();
+const carId = parseInt(route.params.id);
+const car = cars.find((c) => c.id === carId);
+
+const showContact = () => {
+    if (carId === 4) return;
+
+    return router.push(`/cars/${carId}/contact`);
+};
+</script>
+
+<template>
+    <div v-if="car">
+        <h1>Car View</h1>
+        <p>{{ car.name }}</p>
+        <p>{{ car.year }}</p>
+        <p>{{ car.price }}</p>
+        <button @click="showContact">Click for Contact</button>
+        <RouterView />
+    </div>
+    <div v-else>
+        <h1>Car not found</h1>
+    </div>
+</template>
