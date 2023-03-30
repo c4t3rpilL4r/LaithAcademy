a8be2d374f678f36514aad735db55ff01fb80fbb
Raphael de Arce <32305152+c4t3rpilL4r@users.noreply.github.com>
2023-03-30 20:35:54 +0800

    RouterVueLearning - DONE

--- Router Vue Learning/RouterVueLearning/src/router/index.js
+++ Router Vue Learning/RouterVueLearning/src/router/index.js
@@ -7,35 +7,39 @@ import ContactView from "../views/ContactView.vue";
 import NotFoundView from "../views/404View.vue";
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: "/",
-            name: "home",
-            component: HomeView,
-        },
-        {
-            path: "/about",
-            name: "about",
-            component: AboutView,
-        },
-        {
-            path: "/cars/:id",
-            name: "car",
-            component: CarView,
-            children: [
-                {
-                    path: "contact",
-                    component: ContactView,
-                },
-            ],
-        },
-        {
-            path: "/:catchall(.*)",
-            name: "Not Found",
-            component: NotFoundView,
-        },
-    ],
+	history: createWebHistory(import.meta.env.BASE_URL),
+	routes: [
+		{
+			path: "/",
+			name: "home",
+			component: HomeView,
+		},
+		{
+			path: "/home",
+			redirect: "/",
+		},
+		{
+			path: "/about",
+			name: "about",
+			component: AboutView,
+		},
+		{
+			path: "/cars/:id",
+			name: "car",
+			component: CarView,
+			children: [
+				{
+					path: "contact",
+					component: ContactView,
+				},
+			],
+		},
+		{
+			path: "/:catchall(.*)",
+			name: "Not Found",
+			component: NotFoundView,
+		},
+	],
 });
 
 export default router;
