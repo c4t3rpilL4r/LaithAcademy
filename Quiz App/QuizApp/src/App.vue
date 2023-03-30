8bb8341e2bc9f4ae69d7c9721fa32e8dafba7d06
Raphael de Arce <56951197+mllrdev-raphael-dearce@users.noreply.github.com>
2023-03-30 10:40:26 +0800

    refactor: remove commented codes

--- Quiz App/QuizApp/src/App.vue
+++ Quiz App/QuizApp/src/App.vue
@@ -22,13 +22,6 @@ watch(search, () => {
 
         <div class="options-container">
             <Card v-for="quiz in quizzes" :key="quiz.id" :quiz="quiz" />
-            <!-- <div v-for="quiz in quizzes" :key="quiz.id" class="card">
-        <img :src="quiz.img" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div> -->
         </div>
     </div>
 </template>
@@ -63,28 +56,4 @@ header input {
     flex-wrap: wrap;
     margin-top: 40px;
 }
-
-/* .card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
-} */
 </style>
