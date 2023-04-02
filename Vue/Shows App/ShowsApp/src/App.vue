d0d399d89fdc31127f4c432bf37b3e383a6122f0
Raphael de Arce <32305152+c4t3rpilL4r@users.noreply.github.com>
2023-04-03 06:57:51 +0800

    wip: 7th video

---
+++ Vue/Shows App/ShowsApp/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import StarWarsCards from "./components/StarWarsCards.vue";
+</script>
+
+<template>
+	<main>
+		<h1>STAR WARS</h1>
+		<Suspense>
+			<template #default>
+				<StarWarsCards />
+			</template>
+			<template #fallback>Loading...</template>
+		</Suspense>
+	</main>
+</template>
+
+<style scoped></style>
