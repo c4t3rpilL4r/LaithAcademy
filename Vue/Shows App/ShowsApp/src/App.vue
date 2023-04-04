2ca7a6bad6ca68b37627825cf87d0c5fbdeee9a5
Raphael de Arce <32305152+c4t3rpilL4r@users.noreply.github.com>
2023-04-04 13:47:29 +0800

    API Requests to Fetch Data Chapter - DONE

---
+++ Vue/Shows App/ShowsApp/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import Hero from "./components/Hero.vue";
+import BreakingBadCards from "./components/BreakingBadCardsSuspense.vue";
+import RickMortyCards from "./components/RickMortyCards.vue";
+import { ref } from "vue";
+
+const isBreakingBad = ref(true);
+</script>
+
+<template>
+    <main>
+        <Hero
+            :isBreakingBad="isBreakingBad"
+            @selectShow="isBreakingBad = !isBreakingBad"
+        />
+        <KeepAlive>
+            <Component
+                :is="isBreakingBad ? BreakingBadCards : RickMortyCards"
+            />
+        </KeepAlive>
+    </main>
+</template>
